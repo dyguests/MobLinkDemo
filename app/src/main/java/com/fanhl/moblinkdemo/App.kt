3b01dc92193cd71bf2dcf7dcc70324d5c462bca8
fanhl <fanhonglin@klicen.com>
2018-12-11 15:41:25 +0800

    ok

--- app/src/main/java/com/fanhl/moblinkdemo/App.kt
+++ app/src/main/java/com/fanhl/moblinkdemo/App.kt
@@ -1,11 +1,42 @@
 package com.fanhl.moblinkdemo
 
+import android.app.Activity
 import android.app.Application
+import android.util.Log
+import android.widget.Toast
 import com.mob.MobSDK
+import com.mob.moblink.MobLink
+import com.mob.moblink.RestoreSceneListener
+import com.mob.moblink.Scene
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        init()
+    }
+
+    private fun init() {
         MobSDK.init(this)
+        MobLink.setRestoreSceneListener(object : RestoreSceneListener {
+            override fun completeRestore(scene: Scene?) {
+                Log.d(TAG, "completeRestore: ")
+                Toast.makeText(this@App, "completeRestore", Toast.LENGTH_LONG).show()
+            }
+
+            override fun willRestoreScene(scene: Scene?): Class<out Activity> {
+                Log.d(TAG, "willRestoreScene: ")
+                Toast.makeText(this@App, "willRestoreScene", Toast.LENGTH_LONG).show()
+                return MainActivity::class.java
+            }
+
+            override fun notFoundScene(scene: Scene?) {
+                Log.d(TAG, "notFoundScene: ")
+                Toast.makeText(this@App, "notFoundScene", Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    companion object {
+        private val TAG = App::class.java.simpleName
     }
 }
--- app/src/main/java/com/fanhl/moblinkdemo/MainActivity.kt
+++ app/src/main/java/com/fanhl/moblinkdemo/MainActivity.kt
@@ -1,12 +1,65 @@
 package com.fanhl.moblinkdemo
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+import com.mob.moblink.ActionListener
+import com.mob.moblink.MobLink
+import com.mob.moblink.Scene
+import com.mob.moblink.SceneRestorable
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), SceneRestorable {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_create.setOnClickListener { create() }
+    }
+
+    /**
+     * 必须重写该方法，防止MobLink在某些情景下无法还原
+     */
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        MobLink.updateNewIntent(getIntent(), this)
+    }
+
+    override fun onReturnSceneData(scene: Scene) {
+        val info = scene.params["info"] as? String ?: return
+        Toast.makeText(this, "info:$info", Toast.LENGTH_LONG).show()
+    }
+
+    private fun create() {
+        val scene = Scene().apply {
+            path = "/demo/a"
+            params = hashMapOf(
+                "info" to et_info.text.toString()
+            )
+        }
+
+        MobLink.getMobID(scene, object : ActionListener<String> {
+            override fun onResult(mobID: String?) {
+                Log.d(TAG, "onResult: mobID:$mobID")
+                share(mobID ?: return)
+            }
+
+            override fun onError(throwable: Throwable?) {
+                Log.e(TAG, "onError: ", throwable)
+            }
+        })
+    }
+
+    private fun share(mobID: String) {
+        et_url.setText("http://a2au.t4m.cn/$mobID")
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
     }
 }
